5164fa0ec715de9b7571ffa5599b703d40fc00b0
yjhan99 <yjhan99@kaist.ac.kr>
2023-09-01 08:53:17 +0000

    resnet code debugging

--- multimodal_classfiers/resnet.py
+++ multimodal_classfiers/resnet.py
@@ -28,25 +28,25 @@ class ClassifierResnet(Classifier):
             conv_x = keras.layers.Conv1D(filters=int(filters[channel_id] * 2),
                                          kernel_size=int(kernel_size_multipliers[channel_id] * 8), padding='same')(
                 current_layer)
-            conv_x = keras.layers.normalization.BatchNormalization()(conv_x)
+            conv_x = keras.layers.BatchNormalization()(conv_x)
             conv_x = keras.layers.Activation('relu')(conv_x)
 
             conv_y = keras.layers.Conv1D(filters=int(filters[channel_id] * 2),
                                          kernel_size=int(kernel_size_multipliers[channel_id] * 5), padding='same')(
                 conv_x)
-            conv_y = keras.layers.normalization.BatchNormalization()(conv_y)
+            conv_y = keras.layers.BatchNormalization()(conv_y)
             conv_y = keras.layers.Activation('relu')(conv_y)
 
             conv_z = keras.layers.Conv1D(filters=int(filters[channel_id] * 2),
                                          kernel_size=int(kernel_size_multipliers[channel_id] * 3), padding='same')(
                 conv_y)
-            conv_z = keras.layers.normalization.BatchNormalization()(conv_z)
+            conv_z = keras.layers.BatchNormalization()(conv_z)
 
             shortcut_y = current_layer
             if depth == 2:
                 shortcut_y = keras.layers.Conv1D(filters=filters[channel_id] * 2, kernel_size=1, padding='same')(
                     shortcut_y)
-            shortcut_y = keras.layers.normalization.BatchNormalization()(shortcut_y)
+            shortcut_y = keras.layers.BatchNormalization()(shortcut_y)
 
             output_block_3 = keras.layers.add([shortcut_y, conv_z])
             output_block_3 = keras.layers.Activation('relu')(output_block_3)
@@ -69,21 +69,21 @@ class ClassifierResnet(Classifier):
     def build_bloc(self, n_feature_maps, kernel_size_multiplier, input_layer):
         conv_x = keras.layers.Conv1D(filters=n_feature_maps, kernel_size=int(kernel_size_multiplier * 8),
                                      padding='same')(input_layer)
-        conv_x = keras.layers.normalization.BatchNormalization()(conv_x)
+        conv_x = keras.layers.BatchNormalization()(conv_x)
         conv_x = keras.layers.Activation('relu')(conv_x)
 
         conv_y = keras.layers.Conv1D(filters=n_feature_maps, kernel_size=int(kernel_size_multiplier * 5),
                                      padding='same')(conv_x)
-        conv_y = keras.layers.normalization.BatchNormalization()(conv_y)
+        conv_y = keras.layers.BatchNormalization()(conv_y)
         conv_y = keras.layers.Activation('relu')(conv_y)
 
         conv_z = keras.layers.Conv1D(filters=n_feature_maps, kernel_size=int(kernel_size_multiplier * 3),
                                      padding='same')(conv_y)
-        conv_z = keras.layers.normalization.BatchNormalization()(conv_z)
+        conv_z = keras.layers.BatchNormalization()(conv_z)
 
         # expand channels for the sum
         shortcut_y = keras.layers.Conv1D(filters=n_feature_maps, kernel_size=1, padding='same')(input_layer)
-        shortcut_y = keras.layers.normalization.BatchNormalization()(shortcut_y)
+        shortcut_y = keras.layers.BatchNormalization()(shortcut_y)
 
         output_block_1 = keras.layers.add([shortcut_y, conv_z])
         return keras.layers.Activation('relu')(output_block_1)
