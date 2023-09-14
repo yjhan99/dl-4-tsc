6694ab86c6d1a919c2feca21786dcb29ea0a8a5e
yjhan99 <yjhan99@kaist.ac.kr>
2023-09-14 04:59:49 +0000

    start feature clustering

--- GeneralizedModel/kemoworkresults.py
+++ GeneralizedModel/kemoworkresults.py
@@ -18,7 +18,8 @@ def datasets_metrics():
         setups = [f"it_{it:02d}" for it in range(1)]
         add_baseline(dataset, results)
 
-        for architecture in ['fcnM', 'cnnM', 'mlpLstmM', 'resnetM']:
+        # for architecture in ['fcnM', 'cnnM', 'mlpLstmM', 'resnetM']:
+        for architecture in ['fcnM']:
             # for eval_i in range(10):
             for eval_i in range(5):
                 results += get_result(architecture, dataset, eval_i, setups)
--- PersonalizedModel_Baseline/experiment/clustering/wesadclustering.py
+++ PersonalizedModel_Baseline/experiment/clustering/wesadclustering.py
@@ -1,6 +1,17 @@
 import random
 import math
 
+import numpy as np
+
+from utils.utils import prepare_data
+from utils.loggerwrapper import GLOBAL_LOGGER
+from arpreprocessing.dataset import Dataset
+
+import keras
+import keras_contrib
+
+SIGNALS_LEN = 14
+
 def n_fold_split_cluster_trait(subject_ids, n, dataset_name, seed=5):
     result = []
 
@@ -38,16 +49,73 @@ def n_fold_split_cluster_trait(subject_ids, n, dataset_name, seed=5):
     return result
 
 
+def get_ndft(sampling):
+    if sampling <= 2:
+        return 8
+    if sampling <= 4:
+        return 16
+    if sampling <= 8:
+        return 32
+    if sampling <= 16:
+        return 64
+    if sampling <= 32:
+        return 128
+    if sampling in [70, 64, 65]:
+        return 256
+    raise Exception(f"No such sampling as {sampling}")
+
+
 def n_fold_split_cluster_feature(subject_ids, n, seed=5):
-    result = []
+    path = "../archives/mts_archive"
+    dataset = Dataset("WESAD", None, GLOBAL_LOGGER)
+    x, y, sampling_rate = dataset.load(path, subject_ids, SIGNALS_LEN)
 
     random.seed(seed)
-    subject_ids = list(subject_ids)
 
-    test_sets = [subject_ids[i::n] for i in range(n)]
+    x_test, x_train = [[] for i in range(max(SIGNALS_LEN) + 1)], [[] for i in range(max(SIGNALS_LEN) + 1)]
+    y_test, y_train = [], []
+    sampling_test, sampling_train = sampling_rate, sampling_rate, sampling_rate
 
-    for test_set in test_sets:
-        result.append({"train": test_set, "val": test_set, "test": test_set})
+    for subject_id in subject_ids:
+        for channel_id in range(len(SIGNALS_LEN)):
+            signal = x[channel_id]
+
+            num_rows = len(signal)
+            split_size = num_rows // 5
+
+            combined_list = list(zip(signal, y))
+            random.shuffle(combined_list)
+            shuffled_signal, shuffled_y = zip(*combined_list)
+
+            for i in range(split_size):
+                x_test[channel_id] += shuffled_signal[i]
+            for i in range(split_size,num_rows):
+                x_train[channel_id] += shuffled_signal[i]
+
+        for i in range(split_size):
+            y_test += shuffled_y[i]
+        for i in range(split_size,num_rows):
+            y_train += shuffled_y[i]
 
     random.seed()
+
+    x_train = [np.expand_dims(np.array(x), 2) for x in x_train]
+    x_test = [np.expand_dims(np.array(x), 2) for x in x_test]
+    input_shapes, nb_classes, y_none, y_train, y_test, y_true = prepare_data(x_train, y_train, None, y_test)
+    ndft_arr = [get_ndft(x) for x in sampling_test]
+
+
+
+    result = []
+
+    # random.seed(seed)
+    # subject_ids = list(subject_ids)
+
+    # test_sets = [subject_ids[i::n] for i in range(n)]
+
+    # for test_set in test_sets:
+    #     result.append({"train": test_set, "val": test_set, "test": test_set})
+
+    # random.seed()
     return result
+
